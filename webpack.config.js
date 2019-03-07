2e3d68dd395a659b19514ae8ad7c8decc375f4ac
Michael Rumyantsev <Rumyantsev.Michael@gmail.com>
2019-03-07 22:55:49 +0300

    Change build config. Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: './src/index.ts'
+        index: './src/index.ts',
     },
 
     output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.js',
-        publicPath: '/'
+        publicPath: '/',
     },
 
     resolve: {
-        extensions: ['.ts', '.js', 'json']
+        extensions: ['.ts', '.js', 'json'],
     },
 
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'source-map',
 
     devServer: {
         contentBase: path.join(__dirname),
         open: true,
-        publicPath: '/'
+        publicPath: '/',
     },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'src/index.html',
-            filename: 'index.html'
-        })
-    ],
 
     module: {
         rules: [
@@ -37,20 +30,23 @@ module.exports = {
                 test: /\.ts$/,
                 use: [
                     {
-                        loader: 'babel-loader',
+                        loader: 'awesome-typescript-loader',
                         options: {
-                            cacheDirectory: true,
-                            plugins: [
-                                ['transform-class-properties', {spec: true}],
-                                ['transform-object-rest-spread', { useBuiltIns: true }]
-                            ]
-                        }
+                            useCache: true,
+                            reportFiles: [
+                                'src/**/*.{ts}',
+                            ],
+                        },
                     },
-                    {
-                        loader: 'awesome-typescript-loader'
-                    }
-                ]
-            }
-        ]
-    }
+                ],
+            },
+        ],
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            filename: 'index.html',
+        }),
+    ],
 };
